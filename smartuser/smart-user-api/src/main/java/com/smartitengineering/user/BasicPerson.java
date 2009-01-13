84b8640c35a495b7e8d6f47c0b93ff51f43dd235
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-01-13 09:05:13 +0600

    Add basic person object
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

---
+++ smartuser/smart-user-api/src/main/java/com/smartitengineering/user/BasicPerson.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user;
+
+import com.smartitengineering.domain.AbstractPersistentDTO;
+
+/**
+ *
+ * @author modhu7
+ */
+public class BasicPerson extends AbstractPersistentDTO<BasicPerson>{
+    
+    private String nationalID;
+    private Name name;
+
+    public Name getName() {
+        if(name == null){
+            return new Name();
+        }
+        return name;
+    }
+
+    public void setName(Name name) {
+        if(name == null){
+            return;
+        }
+        this.name = name;
+    }
+
+    public String getNationalID() {
+        if(nationalID==null){
+            return "";
+        }
+        return nationalID;
+    }
+
+    public void setNationalID(String nationalID) {
+        if(nationalID==null){
+            return;
+        }
+        this.nationalID = nationalID;
+    }
+    
+    public boolean isValid() {
+        return true;
+    }
+
+}
