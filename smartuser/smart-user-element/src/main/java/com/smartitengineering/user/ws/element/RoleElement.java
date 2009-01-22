d77a76b8716d17e1b860f0c60056d4eceddba56c
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-01-22 20:03:11 +0600

    Correct the role and privilege element
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-element/src/main/java/com/smartitengineering/user/ws/element/RoleElement.java
+++ smartuser/smart-user-element/src/main/java/com/smartitengineering/user/ws/element/RoleElement.java
@@ -6,11 +6,16 @@
 package com.smartitengineering.user.ws.element;
 
 import com.smartitengineering.user.domain.Role;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author modhu7
  */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class RoleElement {
     private Role role;
 
