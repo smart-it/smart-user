ab93eb5a4625ae2eb740eda79183d2b542197f53
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-01-26 15:48:11 +0600

    Add enums to facilitate exceptions
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

---
+++ smartuser/smart-user-api/src/main/java/com/smartitengineering/user/domain/UniqueConstrainedField.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.domain;
+
+/**
+ *
+ * @author modhu7
+ */
+public enum UniqueConstrainedField {
+    PERSON_NATIONAL_ID,
+    PERSON_EMAIL,
+    USER_USERNAME,
+    OTHER
+}
---
+++ smartuser/smart-user-api/src/main/java/com/smartitengineering/user/service/ExceptionMessage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.service;
+
+/**
+ *
+ * @author modhu7
+ */
+public enum ExceptionMessage {
+    CONSTRAINT_VIOLATION_EXCEPTION,
+    STALE_OBJECT_STATE_EXCEPTION,
+}
