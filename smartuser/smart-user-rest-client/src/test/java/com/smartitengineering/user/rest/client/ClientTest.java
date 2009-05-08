94a796298e5d424925d43993394616ea6d537a12
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-05-08 11:03:41 +0600

    change in test for authentication
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-rest-client/src/test/java/com/smartitengineering/user/rest/client/ClientTest.java
+++ smartuser/smart-user-rest-client/src/test/java/com/smartitengineering/user/rest/client/ClientTest.java
@@ -15,6 +15,7 @@ import com.smartitengineering.user.filter.PersonFilter;
 import com.smartitengineering.user.filter.UserFilter;
 import com.smartitengineering.user.filter.UserPersonFilter;
 import com.smartitengineering.user.rest.client.exception.SmartException;
+import com.smartitengineering.user.rest.client.login.LoginCenter;
 import com.smartitengineering.user.service.ExceptionMessage;
 import com.smartitengineering.user.service.PersonService;
 import com.smartitengineering.user.service.PrivilegeService;
@@ -23,7 +24,6 @@ import com.smartitengineering.user.service.UserPersonService;
 import com.smartitengineering.user.service.UserService;
 import com.smartitengineering.user.service.UserServiceFactory;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -33,8 +33,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.core.UriBuilder;
 import junit.framework.TestCase;
 import org.glassfish.embed.GlassFish;
@@ -135,6 +133,8 @@ public class ClientTest extends TestCase {
     }
 
     public void testResources() {
+        LoginCenter.setUsername("superadmin");
+        LoginCenter.setPassword("superadmin");
         doTestPersonService();
         doTestPrivilegeService();
         doTestRoleService();
