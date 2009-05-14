871483d9c814efcf0013d017f6e9b8c57840a99d
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-05-14 09:56:11 +0600

    Change menu dynamically (2nd approach)
    This approach uses lookup to enable/disable menu item.
    Created a central lookup. when login is don a LoginNode is
    published in the central lookup.
    This approach is still failed too.
    
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/AddUserAction.java
+++ smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/AddUserAction.java
@@ -5,8 +5,14 @@
 package com.smartitengineering.user.ui;
 
 import java.awt.event.ActionEvent;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
 import org.openide.windows.TopComponent;
@@ -19,6 +25,49 @@ public class AddUserAction extends AbstractAction {
     public AddUserAction() {
         super(NbBundle.getMessage(AddUserAction.class, "CTL_AddUserAction"));
         putValue(SMALL_ICON, new ImageIcon(Utilities.loadImage(AddUserTopComponent.ICON_PATH, true)));
+        setEnabled(false);
+
+        LoginNode ln = new LoginNode("", "");
+        CentralLookup.getDefault().setLoginNode(ln);
+
+        //final Lookup lookup = CentralLookup.getDefault();
+        Lookup.Result<LoginNode> result = CentralLookup.getDefault().lookupResult(LoginNode.class);
+
+        Collection<LoginNode> loginNodes = (Collection<LoginNode>) result.allInstances();
+
+        Collection all =CentralLookup.getDefault().lookupAll(LoginNode.class);
+        Logger.getLogger(AddUserAction.class.getName()).warning(
+                "The size of the all lookup result:" + new Integer(all.size()).toString());
+        if (all != null) {
+            Iterator ia = all.iterator();
+            int i=0;
+            while (ia.hasNext()) {
+                Logger.getLogger(AddUserAction.class.getName()).warning(
+                        i++ + "--->" + ia.next());
+            }
+        }else{
+            Logger.getLogger(AddUserAction.class.getName()).warning(
+                "The all lookup returns null" );
+        }
+
+        Logger.getLogger(AddUserAction.class.getName()).warning(
+                "The size of the lookup result:" + new Integer(loginNodes.size()).toString());
+        Logger.getLogger(AddUserAction.class.getName()).warning(
+                result.allInstances().toString());
+
+        result.addLookupListener(new LookupListener() {
+
+            public void resultChanged(LookupEvent e) {
+                Lookup.Result r = (Lookup.Result) e.getSource();
+                Collection c = r.allInstances();
+
+                Logger.getLogger(AddUserAction.class.getName()).warning(
+                        "The size of the lookup result from result change:" + new Integer(c.size()).toString());
+                setEnabled(true);
+                
+            }
+        });
+
     }
 
     public void actionPerformed(ActionEvent evt) {
---
+++ smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/LoginNode.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.ui;
+
+/**
+ *
+ * @author modhu7
+ */
+public class LoginNode{
+
+    private String username;
+    private String password;
+
+    
+    LoginNode(String username,  String password){
+        setUsername(username);
+        setPassword(password);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
