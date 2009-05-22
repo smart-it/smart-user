0fe6dc239944f0211d5eb4cac26f1a4fd2d1cb94
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-05-22 15:06:03 +0600

    fix the lookup listener of login action
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/LoginWindowAction.java
+++ smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/LoginWindowAction.java
@@ -4,12 +4,19 @@
  */
 package com.smartitengineering.user.ui;
 
-import com.smartitengineering.user.rest.client.login.LoginCenter;
+import com.smartitengineering.user.ui.*;
 import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
 import org.openide.windows.TopComponent;
@@ -19,9 +26,29 @@ import org.openide.windows.TopComponent;
  */
 public class LoginWindowAction extends AbstractAction {
 
+    Lookup.Result<LoginNode> result = CentralLookup.getDefault().lookupResult(LoginNode.class);
+
     public LoginWindowAction() {
         super(NbBundle.getMessage(LoginWindowAction.class, "CTL_LoginWindowAction"));
-        putValue(SMALL_ICON, new ImageIcon(Utilities.loadImage(LoginWindowTopComponent.ICON_PATH, true)));        
+        putValue(SMALL_ICON, new ImageIcon(Utilities.loadImage(LoginWindowTopComponent.ICON_PATH, true)));
+        setEnabled(true);
+
+        result.addLookupListener(new LookupListener() {
+
+            public void resultChanged(LookupEvent e) {
+                Logger.getLogger(LoginWindowAction.class.getName()).warning("result changed");
+                Lookup.Result r = (Lookup.Result) e.getSource();
+                List<LoginNode> listLoginNode = new ArrayList(r.allInstances());
+                if (listLoginNode!=null && listLoginNode.size()>0) {
+                    if (listLoginNode.get(0).getUsername().equalsIgnoreCase("")) {
+                        setEnabled(true);
+                    } else {
+                        setEnabled(false);
+                    }
+                }
+
+            }
+        });
     }
 
     public void actionPerformed(ActionEvent evt) {
