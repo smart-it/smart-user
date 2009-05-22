7a784694fee7b94398c35d7f3d142b32bbc3f551
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-05-22 15:13:05 +0600

    fix the lookup listener of logout action
    Also change the parent class. Now the parent javax.swing.AbstractAction.
    This change has no significance.
    
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/LogoutAction.java
+++ smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/LogoutAction.java
@@ -4,31 +4,49 @@
  */
 package com.smartitengineering.user.ui;
 
-import org.openide.util.HelpCtx;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
-import org.openide.util.actions.CallableSystemAction;
+import org.openide.util.Utilities;
 
-public final class LogoutAction extends CallableSystemAction {
+public final class LogoutAction extends AbstractAction {
 
-    public void performAction() {
-        // TODO implement action body
+    static final String ICON_PATH = "com/smartitengineering/user/ui/logout.gif";
+    Lookup.Result<LoginNode> result = CentralLookup.getDefault().lookupResult(LoginNode.class);
+
+    public LogoutAction() {
+        super(NbBundle.getMessage(LoginWindowAction.class, "CTL_LogoutAction"));
+        putValue(SMALL_ICON, new ImageIcon(Utilities.loadImage(ICON_PATH, true)));
+
+        setEnabled(false);              
+
+        result.addLookupListener(new LookupListener() {
+
+            public void resultChanged(LookupEvent e) {
+                //Logger.getLogger(LogoutAction.class.getName()).warning("result changed");
+                Lookup.Result r = (Lookup.Result) e.getSource();
+                List<LoginNode> listLoginNode = new ArrayList(r.allInstances());
+                if (listLoginNode!=null && listLoginNode.size()>0) {
+                    if (listLoginNode.get(0).getUsername().equalsIgnoreCase("")) {
+                        setEnabled(false);
+                    } else {
+                        setEnabled(true);
+
+                    }
+                }
+
+            }
+        });
     }
 
-    public String getName() {
-        return NbBundle.getMessage(LogoutAction.class, "CTL_LogoutAction");
+    public void actionPerformed(ActionEvent e) {
+        CentralLookup.getDefault().setLoginNode(new LoginNode("", ""));
     }
 
-    @Override
-    protected String iconResource() {
-        return "com/smartitengineering/user/ui/logout.gif";
-    }
-
-    public HelpCtx getHelpCtx() {
-        return HelpCtx.DEFAULT_HELP;
-    }
-
-    @Override
-    protected boolean asynchronous() {
-        return false;
-    }
 }
