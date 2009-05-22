c2b082b84a1b33d074025c1b623730b91ffc5691
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-05-22 15:02:48 +0600

    fix the lookup listener of adduser action
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

--- smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/AddUserAction.java
+++ smartuser/smart-user-ui/module/src/main/java/com/smartitengineering/user/ui/AddUserAction.java
@@ -4,9 +4,12 @@
  */
 package com.smartitengineering.user.ui;
 
+import com.smartitengineering.user.ui.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
@@ -29,33 +32,6 @@ public class AddUserAction extends AbstractAction {
         putValue(SMALL_ICON, new ImageIcon(Utilities.loadImage(AddUserTopComponent.ICON_PATH, true)));
         setEnabled(false);
 
-        LoginNode ln = new LoginNode("", "");
-        CentralLookup.getDefault().setLoginNode(ln);
-
-        //final Lookup lookup = CentralLookup.getDefault();
-        
-
-        Collection<LoginNode> loginNodes = (Collection<LoginNode>) result.allInstances();
-
-        Collection all =CentralLookup.getDefault().lookupAll(LoginNode.class);
-        Logger.getLogger(AddUserAction.class.getName()).warning(
-                "The size of the all lookup result:" + new Integer(all.size()).toString());
-        if (all != null) {
-            Iterator ia = all.iterator();
-            int i=0;
-            while (ia.hasNext()) {
-                Logger.getLogger(AddUserAction.class.getName()).warning(
-                        i++ + "--->" + ia.next());
-            }
-        }else{
-            Logger.getLogger(AddUserAction.class.getName()).warning(
-                "The all lookup returns null" );
-        }
-
-        Logger.getLogger(AddUserAction.class.getName()).warning(
-                "The size of the lookup result:" + new Integer(loginNodes.size()).toString());
-        Logger.getLogger(AddUserAction.class.getName()).warning(
-                result.allInstances().toString());
 
         result.addLookupListener(new LookupListener() {
 
@@ -65,8 +41,16 @@ public class AddUserAction extends AbstractAction {
 
                 Logger.getLogger(AddUserAction.class.getName()).warning(
                         "The size of the lookup result from result change:" + new Integer(c.size()).toString());
-                setEnabled(true);
-                
+                List<LoginNode> listLoginNode = new ArrayList<LoginNode>(c);
+
+                if (listLoginNode!=null && listLoginNode.size()>0) {
+                    if (listLoginNode.get(0).getUsername().equalsIgnoreCase("")) {
+                        setEnabled(false);
+                    } else {
+                        setEnabled(true);
+                    }
+                }
+
             }
         });
 
