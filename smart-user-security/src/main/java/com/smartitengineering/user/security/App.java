eba3d295e2ec205ac3dad4912decb27966ad8263
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-06-26 11:46:12 +0700

    Create new module for implementing security
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

---
+++ smart-user-security/src/main/java/com/smartitengineering/user/security/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.user.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
