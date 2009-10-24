05488348c86160b6936ddcdd696fcd2d08d72c6b
Subrata Sen Gupta <subrata@smartitengineering.com>
2009-10-25 02:22:50 +0700

    Add voting config material
    Signed-off-by: Subrata Sen Gupta <subrata@smartitengineering.com>

---
+++ smart-user-security/src/main/java/com/smartitengineering/user/security/acl/impl/VotingConfigProvider.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.security.acl.impl;
+
+import java.util.List;
+import org.springframework.security.vote.AccessDecisionVoter;
+
+/**
+ *
+ * @author modhu7
+ */
+public interface VotingConfigProvider {
+
+    public List<VotingConfig> getVotingConfigList();
+
+}
