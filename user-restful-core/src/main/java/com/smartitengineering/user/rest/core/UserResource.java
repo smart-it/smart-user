2dd2bf05e796455c067ed624d203c4d40341a77f
= <russel@russel-laptop.(none)>
2010-07-03 17:15:25 +0600

    User restful core
    Signed-off-by: = <russel@russel-laptop.(none)>

---
+++ user-restful-core/src/main/java/com/smartitengineering/user/rest/core/UserResource.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.rest.core;
+
+/**
+ *
+ * @author russel
+ */
+
+
+public class UserResource {
+
+}
