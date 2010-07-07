013848cec3bc74974e7edb4ab31a08b8ca49a5e1
SSGupta <subrata@smartitengineering.com>
2010-07-07 09:46:15 +0600

    Add Organization resource
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

---
+++ user-restful-core/src/main/java/com/smartitengineering/user/rest/core/OrganizationResource.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.rest.core;
+
+/**
+ *
+ * @author modhu7
+ */
+public class OrganizationResource {
+
+}
