81f2002a393d22486eba0eb657df59e61db891f8
SSGupta <subrata@smartitengineering.com>
2010-08-06 19:36:15 +0600

    Reactiviate the roles and roles in users
    Role, Again!!! but now in a different style.
    Role is mainly a string which is a config attribute of spring security.
    Role is needed as global role of a system. Let's see how it goes!!
    
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

--- smart-user-api/src/main/java/com/smartitengineering/user/domain/Role.java
+++ smart-user-api/src/main/java/com/smartitengineering/user/domain/Role.java
@@ -21,13 +21,8 @@ public class Role extends AbstractPersistentDTO<Role> {
     private Organization parentOrganization;
 
     private String name;
-    private String displayName;
+    private String displayName;    
     private String shortDescription;
-    private Set<Privilege> privileges;
-    private Set<Role> roles;
-
-    private List<Integer> roleIDs;
-    private List<Integer> privilegeIDs;
     private Integer parentOrganizationID;
     
     private Date lastModifiedDate;
@@ -63,56 +58,6 @@ public class Role extends AbstractPersistentDTO<Role> {
         this.shortDescription = shortDescription;
     }
 
-
-    @JsonIgnore
-    public Set<Role> getRoles() {
-        if(roles == null){
-            roles = new HashSet<Role>();
-        }
-        return roles;
-    }
-
-    @JsonIgnore
-    public void setRoles(Set<Role> roles) {
-        if(roles==null){
-            return;
-        }
-        this.roles = roles;
-    }
-
-    @JsonIgnore
-    public Set<Privilege> getPrivileges() {
-        if (privileges == null) {
-            privileges = new HashSet<Privilege>();
-        }
-        return privileges;
-    }
-
-    @JsonIgnore
-    public void setPrivileges(Set<Privilege> privileges) {
-        if (privileges == null) {
-            return;
-        }
-        this.privileges = privileges;
-    }
-
-    @JsonIgnore
-    public List<Integer> getPrivilegeIDs() {
-        return privilegeIDs;
-    }
-
-    public void setPrivilegeIDs(List<Integer> privilegeIDs) {
-        this.privilegeIDs = privilegeIDs;
-    }
-
-    @JsonIgnore
-    public List<Integer> getRoleIDs() {
-        return roleIDs;
-    }
-
-    public void setRoleIDs(List<Integer> roleIDs) {
-        this.roleIDs = roleIDs;
-    }
 
     public String getDisplayName() {
         if (displayName == null) {
