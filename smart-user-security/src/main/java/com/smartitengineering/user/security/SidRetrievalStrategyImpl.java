db520bbfbfea2ae20dc51c154451cf220a2c7458
SSGupta <subrata@smartitengineering.com>
2010-08-06 14:13:00 +0600

    Add retrieval strategy of sid and oid
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

---
+++ smart-user-security/src/main/java/com/smartitengineering/user/security/SidRetrievalStrategyImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.security;
+
+import com.smartitengineering.user.security.acl.impl.SidImpl;
+import org.springframework.security.Authentication;
+import org.springframework.security.acls.sid.Sid;
+import org.springframework.security.acls.sid.SidRetrievalStrategy;
+import org.springframework.security.userdetails.UserDetails;
+
+/**
+ *
+ * @author modhu7
+ */
+public class SidRetrievalStrategyImpl implements SidRetrievalStrategy{
+
+    public Sid[] getSids(Authentication authentication) {
+        
+        Sid[] sids = new Sid[1];
+        if(authentication.getPrincipal() instanceof UserDetails)
+        sids[0] = new SidImpl(((UserDetails)authentication.getPrincipal()).getUsername());
+        
+        return sids;
+    }
+
+}
