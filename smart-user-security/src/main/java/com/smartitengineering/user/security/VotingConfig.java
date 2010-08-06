0b01bec83f5439b7241ec5acbc63c572a6c76927
SSGupta <subrata@smartitengineering.com>
2010-08-06 13:17:33 +0600

    Add the voting config provider
    These configurations contains the permission mask against the different
    config attributes of spring security.
    
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

---
+++ smart-user-security/src/main/java/com/smartitengineering/user/security/VotingConfig.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.security;
+
+import org.springframework.security.ConfigAttribute;
+import org.springframework.security.acls.Permission;
+
+/**
+ *
+ * @author modhu7
+ */
+public class VotingConfig {
+    private ConfigAttribute processConfigAttribute;
+    private Permission[] requirePermission;
+    private Class processDomainObjectClass;
+
+    public Class getProcessDomainObjectClass() {
+        return processDomainObjectClass;
+    }
+
+    public void setProcessDomainObjectClass(Class processDomainObjectClass) {
+        this.processDomainObjectClass = processDomainObjectClass;
+    }
+
+    public ConfigAttribute getProcessConfigAttribute() {
+        return processConfigAttribute;
+    }
+
+    public void setProcessConfigAttribute(ConfigAttribute processConfigAttribute) {
+        this.processConfigAttribute = processConfigAttribute;
+    }
+
+    public Permission[] getRequirePermission() {
+        return requirePermission;
+    }
+
+    public void setRequirePermission(Permission[] requirePermission) {
+        this.requirePermission = requirePermission;
+    }
+
+}
