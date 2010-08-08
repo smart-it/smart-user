1c607429f92da0c19e39f1228e3477e960882dcd
MKRussel <russel@smartitengineering.com>
2010-08-08 10:18:10 +0600

    added api for client impl
    Signed-off-by: MKRussel <russel@smartitengineering.com>

---
+++ smart-user-client-impl/src/main/java/com/smartitengineering/user/client/api/OrganizationSecuredObjectResource.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.client.api;
+
+/**
+ *
+ * @author russel
+ */
+public interface OrganizationSecuredObjectResource {
+
+  
+}
