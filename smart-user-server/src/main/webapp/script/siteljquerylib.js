cb203e761f8b2197edb13d497b718cda81cbe162
Saumitra <saumitra@saumitra-laptop.(none)>
2010-08-22 12:54:10 +0600

    Added pagination plugins
    Signed-off-by: Saumitra <saumitra@saumitra-laptop.(none)>

---
+++ smart-user-server/src/main/webapp/script/siteljquerylib.js
@@ -0,0 +1,23 @@
+jQuery.fn.pagination = function(url,id) {
+  url = url.toString();
+  var prevUrl = url;
+  url = url.replace("?","/frags?");
+  if(url == prevUrl)
+    url = url+"/frags";
+  alert(url);
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "text/html",
+    success: function(html) {      
+      $(id).html(html);     
+
+    },
+    error: function(xhr){
+      alert(window.location);
+      alert(xhr.status);
+
+    }
+        
+  });
+};
