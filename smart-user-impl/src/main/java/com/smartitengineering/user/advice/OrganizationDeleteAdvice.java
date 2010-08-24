6c9a616bac7c1220355c54a4f4d5c703ef395421
SSGupta <subrata@smartitengineering.com>
2010-08-24 13:46:18 +0600

    create org delte advice
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

---
+++ smart-user-impl/src/main/java/com/smartitengineering/user/advice/OrganizationDeleteAdvice.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.advice;
+
+/**
+ *
+ * @author modhu7
+ */
+public class OrganizationDeleteAdvice {
+
+}
