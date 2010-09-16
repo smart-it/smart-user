f109e8ecabe5aec0e78ac9f585c682c8bf643503
SSGupta <subrata@smartitengineering.com>
2010-09-16 11:42:45 +0600

    Add block for test writing
    Signed-off-by: SSGupta <subrata@smartitengineering.com>

--- smart-user-client-impl/src/test/java/com/smartitengineering/clientrest/smartuserclientimpl/ComprehensiveClientTest.java
+++ smart-user-client-impl/src/test/java/com/smartitengineering/clientrest/smartuserclientimpl/ComprehensiveClientTest.java
@@ -2,11 +2,15 @@ package com.smartitengineering.clientrest.smartuserclientimpl;
 
 import com.smartitengineering.smartuser.client.api.LoginResource;
 import com.smartitengineering.smartuser.client.api.OrganizationResource;
+import com.smartitengineering.smartuser.client.api.OrganizationsResource;
 import com.smartitengineering.smartuser.client.api.RootResource;
 import com.smartitengineering.user.client.impl.RootResourceImpl;
+import com.smartitengineering.user.client.impl.domain.Organization;
 import com.smartitengineering.user.client.impl.login.LoginCenter;
 import com.smartitengineering.util.rest.client.ApplicationWideClientFactoryImpl;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -65,4 +69,21 @@ public class ComprehensiveClientTest {
     OrganizationResource orgResource = loginResource.getOrganizationResource();
     Assert.assertNotNull(orgResource);
   }
+
+//Test Started by Uzzal
+  @Test
+  public void doTestCreateOrganization(LoginResource loginResource) {
+    OrganizationsResource orgsResource = loginResource.getOrganizationsResource();
+    Organization org = new Organization();
+    List<OrganizationResource> orgResourceList = new ArrayList<OrganizationResource>(orgsResource.
+        getOrganizationResources());
+    Assert.assertTrue(orgResourceList.size() == 1);
+  }
+
+  @Test
+  public void doTestUpdateOrganization() {
+  }
+  //Test Ended by Uzzal
+  //Test Method Started by Atiqul
+  //Test Ended by Atiqul
 }
