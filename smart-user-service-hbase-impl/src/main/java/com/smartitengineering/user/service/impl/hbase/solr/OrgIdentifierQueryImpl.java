d32c6b9fa142662ae3dbb8913084e41886f2a5e1
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-01-16 18:00:31 +0600

    Organization and user adapter added
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

---
+++ smart-user-service-hbase-impl/src/main/java/com/smartitengineering/user/service/impl/hbase/solr/OrgIdentifierQueryImpl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.user.service.impl.hbase.solr;
+import com.smartitengineering.common.dao.search.solr.spi.ObjectIdentifierQuery;
+import com.smartitengineering.user.domain.Organization;
+import org.apache.solr.client.solrj.util.ClientUtils;
+
+/**
+ *
+ * @author saumitra
+ */
+public class OrgIdentifierQueryImpl implements ObjectIdentifierQuery<Organization>{
+
+  @Override
+  public String getQuery(Organization object) {
+    return new StringBuilder("id: org\\:").append(ClientUtils.escapeQueryChars(object.getId().toString())).toString();
+  }
+}
