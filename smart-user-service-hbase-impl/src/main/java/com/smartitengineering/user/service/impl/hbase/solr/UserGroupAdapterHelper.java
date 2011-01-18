b095f9d0c108ae4b239f98d2da30dd385bf9b11e
Saumitra Chowdhury <saumitra@smartitengineering.com>
2011-01-18 19:30:20 +0600

    UserGroupMulti Adapter Helper added
    Signed-off-by: Saumitra Chowdhury <saumitra@smartitengineering.com>

--- smart-user-service-hbase-impl/src/main/java/com/smartitengineering/user/service/impl/hbase/solr/UserGroupAdapterHelper.java
+++ smart-user-service-hbase-impl/src/main/java/com/smartitengineering/user/service/impl/hbase/solr/UserGroupAdapterHelper.java
@@ -7,42 +7,29 @@ package com.smartitengineering.user.service.impl.hbase.solr;
 
 import com.smartitengineering.dao.solr.MultivalueMap;
 import com.smartitengineering.dao.solr.impl.MultivalueMapImpl;
-import com.smartitengineering.user.domain.User;
 import com.smartitengineering.user.domain.UserGroup;
 import com.smartitengineering.util.bean.adapter.AbstractAdapterHelper;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author saumitra
  */
-public class UserGroupAdapterHelper extends AbstractAdapterHelper<UserGroup, List<MultivalueMap<String, Object>>>{
+public class UserGroupAdapterHelper extends AbstractAdapterHelper<UserGroup, MultivalueMap<String, Object>>{
 
-  public static final String PREFIX = "userGroup:";
-  private static final int PREFIX_INDEX = PREFIX.length();
   @Override
-  protected List<MultivalueMap<String, Object>> newTInstance() {
-    return new ArrayList<MultivalueMap<String, Object>>();
+  protected MultivalueMap<String, Object> newTInstance() {
+    return new MultivalueMapImpl<String, Object>();
   }
 
   @Override
   protected void mergeFromF2T(UserGroup fromBean,
-                              List<MultivalueMap<String, Object>> toBean) {
-    int index = 1;
-    for (User user : fromBean.getUsers()) {
-      MultivalueMap<String, Object> multiValueMap = new MultivalueMapImpl<String, Object>();
-      multiValueMap.addValue("id", (new StringBuilder(PREFIX).append(fromBean.getId().toString()).append(":").append(index)).toString());
-      multiValueMap.addValue("userName", user.getUsername());
-      multiValueMap.addValue("organization", user.getOrganization());
-      toBean.add(multiValueMap);
-      index ++;
-    }
+                              MultivalueMap<String, Object> toBean) {
+    
   }
 
   @Override
-  protected UserGroup convertFromT2F(List<MultivalueMap<String, Object>> toBean) {
-    throw new UnsupportedOperationException("Not supported yet.");
+  protected UserGroup convertFromT2F(MultivalueMap<String, Object> toBean) {
+    return null;
   }
 
 }
